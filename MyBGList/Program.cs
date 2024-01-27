0892f03c214a833c67d7a341649f3eb78863e0b9
Carlos Santos <casantosmu@gmail.com>
2024-01-27 13:40:09 +0100

    Add GetBoardGames

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -10,16 +10,26 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Configuration.GetValue<bool>("UseSwagger"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Minimal API
+app.MapGet("/error", () => Results.Problem());
+app.MapGet("/error/test", () => { throw new Exception("test"); });
+
+// Controllers
 app.MapControllers();
 
 app.Run();
